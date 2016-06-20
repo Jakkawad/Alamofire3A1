a1aee344a7cdd3e71bb2fd1f2c9f5a53d9faea91
Jakkawad <jakkawadchaiplee@Jakkawads-MacBook-Pro.local>
2016-06-20 16:30:51 +0700

    LoadMore
    LoadMore

--- Alamofire3A1/ViewController.swift
+++ Alamofire3A1/ViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView:UITableView!
+    
     var mainDataArray = NSArray()
     var jsonArray:NSMutableArray?
     var newArray: Array<String> = []
     
+    //Number
+    var numberToAdd = 10
+    var numberLimit = 100
+    var numberCurrent = 40
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 50
+        return mainDataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell0 = tableView.dequeueReusableCellWithIdentifier("tableCell0")
+        let item = mainDataArray[indexPath.row] as! NSDictionary
+        cell0?.textLabel?.text = item.objectForKey("ProductName") as? String
+        return cell0!
+    }
+    
     func loadData() {
-        Alamofire.request(.POST, "https://www.all2sale.com/sendmail/testfunction/json/apitest.php", parameters: ["api":"productall","productall":"30"])
+        Alamofire.request(.POST, "https://www.all2sale.com/sendmail/testfunction/json/apitest.php", parameters: ["api":"product_rand","product_rand":numberCurrent])
             .responseJSON { response in
                 //print(response.request)  // original URL request
                 //print(response.response) // URL response
@@ -25,8 +48,31 @@ class ViewController: UIViewController {
                 
                 // Array
                 self.mainDataArray = response.result.value as! NSArray
-                //print(self.mainDataArray)
+                print("COUNT\(self.mainDataArray.count)")
+                self.tableView.reloadData()
                 
+        }
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let currentOffset = scrollView.contentOffset.y;
+        let maximumOffset = scrollView.contentSize.height - scrollView.frame.size.height;
+        
+        //NSInteger result = maximumOffset - currentOffset;
+        
+        // Change 10.0 to adjust the distance from bottom
+        if (maximumOffset - currentOffset <= 10.0) {
+            //print("ScrollView")
+            //[self methodThatAddsDataAndReloadsTableView];
+            if numberCurrent == numberLimit {
+                print("Out of RANG!!")
+            } else {
+                numberCurrent += numberToAdd
+                
+                print("NumberCurrent\(numberCurrent)")
+                loadData()
+                self.tableView.reloadData()
+            }
         }
     }
     
